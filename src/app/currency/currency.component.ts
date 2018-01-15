7a85e6ff31a7c088ea02ef7ba6ee827664eace0d
Chartchai Doungsa-ard <chartchai.d@cmu.ac.th>
2018-01-16 04:33:23 +0700

    third bug the result rate is in thai

--- src/app/currency/currency.component.ts
+++ src/app/currency/currency.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {isUndefined} from 'util';
-import {ActivatedRoute} from '@angular/router';
+import { isUndefined } from 'util';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-currency',
@@ -8,49 +8,54 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./currency.component.css']
 })
 export class CurrencyComponent implements OnInit {
-  title:string;
-  currencyList:string[];
-  currentSource:string;
-  currentTarget:string;
-  outputTarget:string;
-  result:number;
-  calculate:boolean;
-  sourceAmount:number;
-  outputClass:string;
-  constructor(private route: ActivatedRoute) {}
+  title: string;
+  currencyList: string[];
+  currencyListTH: string[];
+  currentSource: string;
+  currentTarget: string;
+  outputTarget: string;
+  result: number;
+  calculate: boolean;
+  sourceAmount: number;
+  outputClass: string;
+  constructor(private route: ActivatedRoute) { }
   ngOnInit() {
     this.currencyList = [];
     this.title = 'Currency Change';
     this.currencyList.push('THB');
     this.currencyList.push('USD');
     this.currencyList.push('EUR');
+    this.currencyListTH = [];
+    this.currencyListTH.push('บาท');
+    this.currencyListTH.push('ดอลลาร์');
+    this.currencyListTH.push('ยูโร');
     this.currentSource = this.currencyList[0];
     this.currentTarget = this.currencyList[0];
     this.result = null;
-    this.calculate=false;
+    this.calculate = false;
     // this.sourceAmount = 0;
     this.outputClass = 'alert alert-success';
   }
 
-  onCurrentSourceClick(currency:string){
+  onCurrentSourceClick(currency: string) {
     this.currentSource = currency;
   }
-  onCurrentTargetClick(currency:string){
+  onCurrentTargetClick(currency: string) {
     this.currentTarget = currency;
   }
 
-  onCalculated(amount:number){
-    let result:number;
+  onCalculated(amount: number) {
+    let result: number;
     this.calculate = true;
-    let exchangeRates:number[];
+    let exchangeRates: number[];
     this.sourceAmount = +amount;
     exchangeRates = [];
     exchangeRates['THB'] = 33;
-    exchangeRates['USD'] = 1;
-    exchangeRates['EUR'] = 1.01;
-    result = this.sourceAmount * exchangeRates[this.currentTarget]/exchangeRates[this.currentSource];
+    exchangeRates['USD'] = 1.01;
+    exchangeRates['EUR'] = 1;
+    result = this.sourceAmount * exchangeRates[this.currentTarget] / exchangeRates[this.currentSource];
     this.outputTarget = this.currentTarget;
-    this.result = result ;
+    this.result = result;
   }
 
 }
